e5ce6f18dbf3ca637399c9e31132af2b746657af
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2023-05-17 23:21:16 +0530

    Add files via upload

---
+++ Lab4/Application/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public void load(View view) {
+        Button button = (Button) view;
+        TextView textView = findViewById(R.id.textView);
+        button.setText(textView.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
