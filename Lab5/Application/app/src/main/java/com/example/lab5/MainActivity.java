c183dcb1a03bb95f9f6164eabc1a864d4a8f13db
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2023-05-17 23:22:54 +0530

    Add files via upload

---
+++ Lab5/Application/app/src/main/java/com/example/lab5/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void display(View view) {
+        Log.d("disp", "There is an error here");
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
